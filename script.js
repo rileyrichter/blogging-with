d5758cc20516303b3071b3564a61a01dcffd8268
Ben Parker <bparker@gmail.com>
2020-12-13 21:33:33 -0600

    Updated script

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 $(document).ready(function () {
-  $("article").each(function () {
-    let _this = $(this);
-
-    _this.readingTime({
-      readingTimeTarget: _this.find(".eta"),
-      wordCountTarget: _this.find(".words"),
-      remotePath: _this.attr("data-file"),
-      remoteTarget: _this.attr("data-target"),
-    });
-  });
+  $("article").readingTime();
 });
