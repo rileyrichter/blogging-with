e67e35c6f58bbc2243ed1d30524afdae653f9ee8
Ben Parker <bparker@gmail.com>
2020-12-13 21:15:24 -0600

    Updated jquery

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-$("article").each(function () {
-  let _this = $(this);
+$(document).ready(function () {
+  $("article").each(function () {
+    let _this = $(this);
 
-  _this.readingTime({
-    readingTimeTarget: _this.find(".eta"),
-    wordCountTarget: _this.find(".words"),
-    remotePath: _this.attr("data-file"),
-    remoteTarget: _this.attr("data-target"),
+    _this.readingTime({
+      readingTimeTarget: _this.find(".eta"),
+      wordCountTarget: _this.find(".words"),
+      remotePath: _this.attr("data-file"),
+      remoteTarget: _this.attr("data-target"),
+    });
   });
 });
