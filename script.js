9a13ae1f0cad971a506498e576a75ec80e871e06
Ben Parker <bparker@gmail.com>
2020-12-13 21:11:08 -0600

    Added reading time scripts

---
+++ script.js
@@ -0,0 +1,10 @@
+$("article").each(function () {
+  let _this = $(this);
+
+  _this.readingTime({
+    readingTimeTarget: _this.find(".eta"),
+    wordCountTarget: _this.find(".words"),
+    remotePath: _this.attr("data-file"),
+    remoteTarget: _this.attr("data-target"),
+  });
+});
